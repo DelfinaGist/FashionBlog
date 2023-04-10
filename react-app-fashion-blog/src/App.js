7bbb0aea3106c9ef1b512f9a3d003ea140c26ee4
DelfinaGist <fullstackcodez@gmail.com>
2023-04-10 12:40:48 -0500

    committing

---
+++ react-app-fashion-blog/src/App.js
@@ -0,0 +1,16 @@
+import Article from './components/Article'
+import Header from './components/Header';
+import './index.css';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+     <Article />
+     <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app-fashion-blog/src/components/Footer.js
@@ -0,0 +1,21 @@
+export default function Footer () {
+    return (
+        <>
+            <footer>
+        <nav aria-label="Main Navigation" role="navigation">
+            <ul class="nav-list">
+                <li><a className="bot-a" href="...">Home</a></li>
+                <li><a className="bot-a" href="...">Women's</a></li>
+                <li><a className="bot-a" href="...">Men's</a></li>
+                <li><a className="bot-a" href="...">On the Street</a></li>
+                <li><a className="bot-a" href="...">The Catwalk</a></li>
+                <li><a className="bot-a" href="...">AdWatch</a></li>
+                <li><a className="bot-a" href="...">Aboot</a> </li>
+                <li><a className="bot-a" href="...">Tips</a></li>
+            </ul>
+            <h3> &#169; 2013 Valet Industries, Inc</h3>
+        </nav>
+    </footer>  
+        </>
+    )
+}
